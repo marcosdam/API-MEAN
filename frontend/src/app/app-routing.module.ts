6a724afdba4fb72ff13196bbfb1bcc1593cb2c99
marcosdam <marcosledesmapicazo@gmail.com>
2021-01-13 17:56:00 +0100

    Añadidas func getOneMovie & updateMovie & deleteMovie Añadido frontend con Angular & moviesComponent, movie class & movie service

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MoviesComponent} from "./components/movies/movies.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MoviesComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
