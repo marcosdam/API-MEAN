65c3beed7c4d6bd5dca216ae878e91ffd0220910
marcosdam <marcosledesmapicazo@gmail.com>
2021-01-18 20:20:18 +0100

    Añadido CRUD en movie.service.ts

--- frontend/src/app/services/movie.service.ts
+++ frontend/src/app/services/movie.service.ts
@@ -6,8 +6,30 @@ import {HttpClient} from "@angular/common/http";
   providedIn: 'root'
 })
 export class MovieService {
-  // Crear una película
+  // Peli y Lista de pelis
   selectedMovie: Movie;
+  movies: Movie[];
+
+  // URL de la API
+  readonly URL_API = 'http://localhost:3000/api/movies';
+
   // Inyectar protocolo HTTP en el const
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    // Iniciar peli
+    this.selectedMovie = new Movie();
+  }
+
+  // Funciones (GET / POST / PUT / DELETE)
+  getMovies(){
+    return this.http.get(this.URL_API);
+  }
+  postMovie(movie: Movie){
+    return this.http.post(this.URL_API, movie);
+  }
+  putMovie(movie: Movie){
+    return this.http.put(this.URL_API+`/${movie._id}`, movie);
+  }
+  deleteMovie(_id: string){
+    return this.http.delete(this.URL_API+`/${_id}`);
+  }
 }
